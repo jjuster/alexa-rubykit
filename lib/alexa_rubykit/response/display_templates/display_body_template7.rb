b7ec8fb8b7c5654152aba7d0286709a9b7730548
sparkgene <jichikawa@r.recruit.co.jp>
2018-01-18 23:05:59 +0900

    body7

---
+++ lib/alexa_rubykit/response/display_templates/display_body_template7.rb
@@ -0,0 +1,32 @@
+module AlexaRubykit
+
+    require 'alexa_rubykit/response/display_templates/display_template'
+    class DisplayBodyTemplate7 < DisplayTemplate
+
+        require 'alexa_rubykit/response/display_templates/display_text_context'
+
+        attr_accessor :title, :back_button, :background_image
+        attr_accessor :token, :image
+
+        TEMPLATE_NAME       = "BodyTemplate7".freeze
+        def initialize()
+            super
+            @image = []
+            @token = TEMPLATE_NAME + "_token"
+        end
+
+        def to_json
+            directive = Hash.new()
+            directive[:template] = {
+                :type => TEMPLATE_NAME,
+                :token => @token,
+                :backButton => back_button_status,
+                :backgroundImage => @background_image.to_hash,
+                :image => @image.to_hash,
+                :title => @title
+            }
+            directive.to_json
+        end
+
+    end
+end
---
+++ spec/body_template7_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require 'alexa_rubykit/response/display_templates/display_body_template7'
+
+describe 'Request handling' do
+  it 'validate json format' do
+    sample_json = JSON.parse(File.read('fixtures/sample-BodyTemplate7.json'))
+    template = AlexaRubykit::DisplayBodyTemplate7.new
+    template.token = sample_json["template"]["token"]
+    template.back_button = (sample_json["template"]["backButton"] == "VISIBLE")
+    background_image = AlexaRubykit::DisplayImage.new(
+      sample_json["template"]["backgroundImage"]["sources"][0]["url"]
+    )
+    template.background_image = background_image
+    image = AlexaRubykit::DisplayImage.new(
+      sample_json["template"]["image"]["sources"][0]["url"]
+    )
+    template.image = image
+    template_json = JSON.parse(template.to_json)
+
+    expect(template_json["template"]["type"]).to eq('BodyTemplate7')
+
+    expect(template_json["template"]["backgroundImage"]["sources"][0]["url"]).to eq(sample_json["template"]["backgroundImage"]["sources"][0]["url"])
+    expect(template_json["template"]["image"]["sources"][0]["url"]).to eq(sample_json["template"]["image"]["sources"][0]["url"])
+  end
+end
