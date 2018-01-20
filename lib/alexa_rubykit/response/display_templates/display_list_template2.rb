d4d81f3a7de4e0d921a3148080e16d7e9c11ba3f
sparkgene <jichikawa@r.recruit.co.jp>
2018-01-20 13:38:04 +0900

    list2

---
+++ lib/alexa_rubykit/response/display_templates/display_list_template2.rb
@@ -0,0 +1,46 @@
+module AlexaRubykit
+
+    require 'alexa_rubykit/response/display_templates/display_template'
+    class DisplayListTemplate2 < DisplayTemplate
+
+        require 'alexa_rubykit/response/display_templates/display_text_context'
+
+        attr_accessor :title, :back_button, :background_image
+        attr_accessor :token, :list_item
+
+        TEMPLATE_NAME       = "ListTemplate2".freeze
+        def initialize()
+            super
+            @token = TEMPLATE_NAME + "_token"
+            @list_item = []
+        end
+
+        def add_list_item(text_content, token=nil, image=nil)
+            item = Hash.new()
+            item[:textContent] = text_content
+            if token
+                item[:token] = token
+            else
+                item[:token] = "LIST_ITEM_" + @list_item.length.to_s
+            end
+            if token
+                item[:image] = image
+            end
+            @list_item.push(item)
+        end
+
+        def to_json
+            directive = Hash.new()
+            directive[:template] = {
+                :type => TEMPLATE_NAME,
+                :token => @token,
+                :backButton => back_button_status,
+                :backgroundImage => @background_image.to_hash,
+                :title => @title,
+                :listItems => @list_item
+            }
+            directive.to_json
+        end
+
+    end
+end
