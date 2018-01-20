f7f5f53c8a2c13144d7919ac9d7722150c5368be
sparkgene <jichikawa@r.recruit.co.jp>
2018-01-20 16:07:44 +0900

    fix module name

--- lib/alexa_rubykit/response/display_templates/display_body_template1.rb
+++ lib/alexa_rubykit/response/display_templates/display_body_template1.rb
@@ -1,35 +1,37 @@
 module AlexaRubykit
-    module DisplayTemplates
-        require 'alexa_rubykit/response/display_templates/display_template'
-        class DisplayBodyTemplate1 < DisplayTemplate
+    module Response
+        module DisplayTemplates
+            require 'alexa_rubykit/response/display_templates/display_template'
+            class DisplayBodyTemplate1 < DisplayTemplate
 
-            require 'alexa_rubykit/response/display_templates/display_text_context'
+                require 'alexa_rubykit/response/display_templates/display_text_context'
 
-            attr_accessor :title, :back_button, :background_image
-            attr_accessor :token, :text_context
+                attr_accessor :title, :back_button, :background_image
+                attr_accessor :token, :text_context
 
-            TEMPLATE_NAME       = "BodyTemplate1".freeze
-            def initialize()
-                super
-                @token = TEMPLATE_NAME + "_token"
-                @text_context = DisplayTextContext.new()
-            end
+                TEMPLATE_NAME       = "BodyTemplate1".freeze
+                def initialize()
+                    super
+                    @token = TEMPLATE_NAME + "_token"
+                    @text_context = DisplayTextContext.new()
+                end
 
-            def to_hash
-                directive = Hash.new()
-                directive[:template] = {
-                    :type => TEMPLATE_NAME,
-                    :token => @token,
-                    :backButton => back_button_status,
-                    :backgroundImage => @background_image.to_hash,
-                    :title => @title,
-                    :textContent => @text_context.to_hash
-                }
-                directive
-            end
+                def to_hash
+                    directive = Hash.new()
+                    directive[:template] = {
+                        :type => TEMPLATE_NAME,
+                        :token => @token,
+                        :backButton => back_button_status,
+                        :backgroundImage => @background_image.to_hash,
+                        :title => @title,
+                        :textContent => @text_context.to_hash
+                    }
+                    directive
+                end
 
-            def to_json
-                self.to_hash.to_json
+                def to_json
+                    self.to_hash.to_json
+                end
             end
         end
     end
